5fd1609f14c6d4bfe0bd391790ec88d93468d5d4
Sevval-10 <sevvalirmak4@gmail.com>
2025-11-25 23:05:06 +0300

    Add project files.

---
+++ KutuphaneOtomasyonuuuuu/AnaSayfafrm.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace KutuphaneOtomasyonuuuuu
+{
+    partial class AnaSayfafrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btnUyeEkle = new System.Windows.Forms.Button();
+            this.btnUyeListele = new System.Windows.Forms.Button();
+            this.btnKitapEkle = new System.Windows.Forms.Button();
+            this.btnKitapListele = new System.Windows.Forms.Button();
+            this.btnEmanetVer = new System.Windows.Forms.Button();
+            this.btnEmanetListele = new System.Windows.Forms.Button();
+            this.btnEmanetİade = new System.Windows.Forms.Button();
+            this.btnSiralama = new System.Windows.Forms.Button();
+            this.btnGrafik = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnUyeEkle);
+            this.groupBox1.Controls.Add(this.btnUyeListele);
+            this.groupBox1.Location = new System.Drawing.Point(77, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(484, 193);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Üye İşlemleri";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnKitapEkle);
+            this.groupBox2.Controls.Add(this.btnKitapListele);
+            this.groupBox2.Location = new System.Drawing.Point(886, 28);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(484, 193);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kitap İşlemleri";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnEmanetVer);
+            this.groupBox3.Controls.Add(this.btnEmanetListele);
+            this.groupBox3.Controls.Add(this.btnEmanetİade);
+            this.groupBox3.Location = new System.Drawing.Point(77, 269);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(484, 193);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Emanet Kitap İşlemleri";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btnSiralama);
+            this.groupBox4.Controls.Add(this.btnGrafik);
+            this.groupBox4.Location = new System.Drawing.Point(886, 269);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(484, 193);
+            this.groupBox4.TabIndex = 0;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Sıralama ve Grafikler";
+            // 
+            // btnUyeEkle
+            // 
+            this.btnUyeEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUyeEkle.Location = new System.Drawing.Point(25, 51);
+            this.btnUyeEkle.Name = "btnUyeEkle";
+            this.btnUyeEkle.Size = new System.Drawing.Size(430, 30);
+            this.btnUyeEkle.TabIndex = 1;
+            this.btnUyeEkle.Text = "Üye Ekleme İşlemleri";
+            this.btnUyeEkle.UseVisualStyleBackColor = true;
+            // 
+            // btnUyeListele
+            // 
+            this.btnUyeListele.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUyeListele.Location = new System.Drawing.Point(25, 123);
+            this.btnUyeListele.Name = "btnUyeListele";
+            this.btnUyeListele.Size = new System.Drawing.Size(430, 30);
+            this.btnUyeListele.TabIndex = 2;
+            this.btnUyeListele.Text = "Üye Listeleme İşlemleri";
+            this.btnUyeListele.UseVisualStyleBackColor = true;
+            // 
+            // btnKitapEkle
+            // 
+            this.btnKitapEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKitapEkle.Location = new System.Drawing.Point(30, 51);
+            this.btnKitapEkle.Name = "btnKitapEkle";
+            this.btnKitapEkle.Size = new System.Drawing.Size(430, 30);
+            this.btnKitapEkle.TabIndex = 3;
+            this.btnKitapEkle.Text = "Kitap Ekleme İşlemleri";
+            this.btnKitapEkle.UseVisualStyleBackColor = true;
+            // 
+            // btnKitapListele
+            // 
+            this.btnKitapListele.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKitapListele.Location = new System.Drawing.Point(30, 123);
+            this.btnKitapListele.Name = "btnKitapListele";
+            this.btnKitapListele.Size = new System.Drawing.Size(430, 30);
+            this.btnKitapListele.TabIndex = 4;
+            this.btnKitapListele.Text = "Kitap Listeleme İşlemleri";
+            this.btnKitapListele.UseVisualStyleBackColor = true;
+            // 
+            // btnEmanetVer
+            // 
+            this.btnEmanetVer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEmanetVer.Location = new System.Drawing.Point(25, 32);
+            this.btnEmanetVer.Name = "btnEmanetVer";
+            this.btnEmanetVer.Size = new System.Drawing.Size(430, 30);
+            this.btnEmanetVer.TabIndex = 5;
+            this.btnEmanetVer.Text = "Emanet Kitap Verme İşlemleri";
+            this.btnEmanetVer.UseVisualStyleBackColor = true;
+            // 
+            // btnEmanetListele
+            // 
+            this.btnEmanetListele.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEmanetListele.Location = new System.Drawing.Point(25, 93);
+            this.btnEmanetListele.Name = "btnEmanetListele";
+            this.btnEmanetListele.Size = new System.Drawing.Size(430, 30);
+            this.btnEmanetListele.TabIndex = 6;
+            this.btnEmanetListele.Text = "Emanet Kitap Listeleme İşlemleri";
+            this.btnEmanetListele.UseVisualStyleBackColor = true;
+            // 
+            // btnEmanetİade
+            // 
+            this.btnEmanetİade.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEmanetİade.Location = new System.Drawing.Point(25, 146);
+            this.btnEmanetİade.Name = "btnEmanetİade";
+            this.btnEmanetİade.Size = new System.Drawing.Size(430, 30);
+            this.btnEmanetİade.TabIndex = 7;
+            this.btnEmanetİade.Text = "Emanet Kitap İade İşlemleri";
+            this.btnEmanetİade.UseVisualStyleBackColor = true;
+            // 
+            // btnSiralama
+            // 
+            this.btnSiralama.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSiralama.Location = new System.Drawing.Point(30, 61);
+            this.btnSiralama.Name = "btnSiralama";
+            this.btnSiralama.Size = new System.Drawing.Size(430, 30);
+            this.btnSiralama.TabIndex = 8;
+            this.btnSiralama.Text = "Sıralama";
+            this.btnSiralama.UseVisualStyleBackColor = true;
+            // 
+            // btnGrafik
+            // 
+            this.btnGrafik.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGrafik.Location = new System.Drawing.Point(30, 128);
+            this.btnGrafik.Name = "btnGrafik";
+            this.btnGrafik.Size = new System.Drawing.Size(430, 30);
+            this.btnGrafik.TabIndex = 9;
+            this.btnGrafik.Text = "Grafikler";
+            this.btnGrafik.UseVisualStyleBackColor = true;
+            // 
+            // AnaSayfafrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1441, 525);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "AnaSayfafrm";
+            this.Text = "Ana Sayfa";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btnUyeEkle;
+        private System.Windows.Forms.Button btnUyeListele;
+        private System.Windows.Forms.Button btnKitapEkle;
+        private System.Windows.Forms.Button btnKitapListele;
+        private System.Windows.Forms.Button btnEmanetVer;
+        private System.Windows.Forms.Button btnEmanetListele;
+        private System.Windows.Forms.Button btnEmanetİade;
+        private System.Windows.Forms.Button btnSiralama;
+        private System.Windows.Forms.Button btnGrafik;
+    }
+}
+
---
+++ KutuphaneOtomasyonuuuuu/UyeEklefrm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace KutuphaneOtomasyonuuuuu
+{
+    public partial class UyeEklefrm : Form
+    {
+        public UyeEklefrm()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti=new SqlConnection("Data Source=DESKTOP-6SCKJNF;Initial Catalog=KütüphaneOtomasyonu;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+        private void btnİptal_Click(object sender, EventArgs e)
+        {
+            this .Close();
+        }
+       
+        private void btnUyeEkle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into uye(tc,adsoyad,yas,cinsiyet,telefon,adres,email,okukitapsayisi) values(@tc,@adsoyad,@yas,@cinsiyet,@telefon,@adres,@email,@okukitapsayisi)", baglanti);
+            komut.Parameters.AddWithValue("@tc",txtTc.Text);
+            komut.Parameters.AddWithValue("@adsoyad",txtAdSoyad.Text);
+            komut.Parameters.AddWithValue("@yas",txtYas.Text);
+            komut.Parameters.AddWithValue("@cinsiyet",comboCinsiyet.Text);
+            komut.Parameters.AddWithValue("@telefon",txtTelefon.Text);
+            komut.Parameters.AddWithValue("@adres",txtAdres.Text);
+            komut.Parameters.AddWithValue("@email",txtEmail.Text);
+            komut.Parameters.AddWithValue("@okukitapsayisi",txtOkunanSayi.Text);
+
+
+        }
+    }
+}
